ba415d5dd58892dfad8463c3fdcefee4269a1aae
dao.dev <dorothyng284@gmail.com>
2026-01-24 00:35:39 -0700

    change file path

--- topic2/ANN/frontend/app.py
+++ topic2/ANN/frontend/app.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 # Add backend to path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 from backend.model import MNISTModel
 
 # Page configuration
